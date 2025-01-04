4ace666058b1edeef0fd48356a6f619092debacc
saifudheen <saifudheen@innovatise.com>
2025-01-04 11:32:31 +0530

    added test case for input 1

--- src/controller/index.js
+++ src/controller/index.js
@@ -1,9 +1,12 @@
-import { AddNumbers } from "../addNumbers";
+import { AddNumbers } from "../shared/AddNumbers";
 
 
 export class Apis {
     async addNumbers(req, res) {
         const { numberString } = req.query;
+        if(!numberString) {
+            res.status(400).json({ result: 0 })
+        }
         const result = AddNumbers.prototype.addNumbers(numberString);
         res.status(200).json({ result });
     }
--- src/test/AddNumbers.test.js
+++ src/test/AddNumbers.test.js
@@ -1,13 +1,11 @@
 import { Apis } from "../controller";
 
-
+const req = { query: { numberString: "" } };
+const res = { status: jest.fn().mockReturnThis(), json: jest.fn() };
 
 describe("addNumbers", () => {
 
     test("function should return result 0 if empty string passed", async () => {
-        const req = { query: { numberString: "" } };
-        const res = { status: jest.fn().mockReturnThis(), json: jest.fn() };
-
         await Apis.prototype.addNumbers(req, res);
         expect(res.json).toHaveBeenCalledWith({
             result: 0
@@ -15,11 +13,16 @@ describe("addNumbers", () => {
     });
 
     test("function should return 400 response if empty string passed", async () => {
-        const req = { query: { numberString: "" } };
-        const res = { status: jest.fn().mockReturnThis(), json: jest.fn() };
-
         await Apis.prototype.addNumbers(req, res);
         expect(res.status).toHaveBeenCalledWith(400);
+    });
+
+    test('returns the number for a single number string', async () => {
+        await Apis.prototype.addNumbers(req, res);
+        expect(res.status).toHaveBeenCalledWith(200);
+        expect(res.json).toHaveBeenCalledWith({
+            result: 1
+        });
     });
 
 });
