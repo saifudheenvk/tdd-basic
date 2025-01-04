1c216d72aae3964effbdc8bafdcd14e38fb8c872
saifudheen <saifudheen@innovatise.com>
2025-01-04 12:38:10 +0530

    added new delimitter

--- src/shared/AddNumbers.js
+++ src/shared/AddNumbers.js
@@ -6,11 +6,18 @@ export class AddNumbers {
             throw new Error("numberString is required");
         }
         const parsedNumbers = this.parseNumberString(numberString);
+        console.log(parsedNumbers)
         const total = parsedNumbers.reduce((total, number) => total + parseInt(number), 0);
         return total;
     }
 
     parseNumberString(numberString) {
-        return numberString.split(/[\n,]/);
+        let delimitter = /,|\n/;
+        if(numberString.startsWith('//')) {
+            const parts = numberString.split('\n');
+            numberString = parts[1];
+            delimitter = parts[0].slice(2);
+        }
+        return numberString.split(delimitter).filter(number => number!== '');
     }
 }
